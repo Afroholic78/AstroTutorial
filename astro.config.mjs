e1407bf840843bf7f55449b855847768471e54f3
afroholic78 <mickael.zerihoun@gmail.com>
2025-01-03 22:46:27 -0800

    finish tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://main--golden-brigadeiros-6192dd.netlify.app/",
+  integrations: [preact()]
+});
